004d560b3dbb3cd4423bf3f4dc49e82c99c23ed1
Jaagup Tamme <jaaguptamme@gmail.com>
2023-07-19 13:02:54 +0300

    Update SplayTree.cpp

--- SplayTree.cpp
+++ SplayTree.cpp
@@ -1,207 +1,182 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 using namespace std;
-template<typename K,typename V>
-class SplayTree{
-    class SplayNode{public:
-        K voti;
-        V vaartus;
-        SplayNode* l;
-        SplayNode* r;
-        SplayNode* p;
-        SplayNode(){
-            this->l=this->r=this->p=NULL;
-        }
-        bool onLeht(){
-            return (l==NULL)&&(r==NULL);
-        }
-    };
-    SplayNode* juur;
-    V otsi(SplayNode* tipp,K& voti){
-        if(tipp==NULL){
-            V vaartus;
-            return vaartus;
-        }
-        if(tipp->voti==voti){
-            splay(tipp);
-            return tipp->vaartus;
-        }
-        if(tipp->l==NULL && voti<tipp->voti){
-            splay(tipp);
-            V vaartus;
-            return vaartus;
-        }
-        if(tipp->r==NULL && voti>tipp->voti){
-            splay(tipp);
-            V vaartus;
-            return vaartus;
-        }
-        if(voti<tipp->voti)return otsi(tipp->l,voti);
-        return otsi(tipp->r,voti);
-    }
-    void kustuta(SplayNode* tipp, K& voti){
-        if((tipp==NULL)||(tipp->l==NULL && voti<tipp->voti)||(tipp->r==NULL && voti>tipp->voti))splay(tipp);
-        else if(voti>tipp->voti)kustuta(tipp->r,voti);
-        else if(voti<tipp->voti)kustuta(tipp->l,voti);
-        else if(tipp->onLeht()){
-            auto ulem=tipp->p;
-            delete tipp;
-            tipp=nullptr;
-            splay(ulem);
-        }else if(tipp->l==NULL || tipp->r==NULL){
-            if(tipp->l==NULL){
-                auto kustutada=tipp;
-                auto ulem=tipp->p;
-                tipp=tipp->r;
-                tipp->p=ulem;
-                splay(tipp->p);
-                delete kustutada;
-            }else{
-                auto kustutada=tipp;
-                auto ulem=tipp->p;
-                tipp=tipp->l;
-                tipp->p=ulem;
-                splay(tipp->p);
-                delete kustutada;
-            }
-        }else{
-            auto kustutada=tipp->r;
-            while(kustutada->l!=NULL)kustutada=kustutada->l;
-            tipp->voti=kustutada->voti;
-            tipp->vaartus=kustutada->vaartus;
-            kustuta(tipp->r,tipp->voti);
-        }
-    }
-    void insert(SplayNode* tipp,K& voti,V& vaartus){
-        if(tipp==NULL){
-            juur=new SplayNode();
-            juur->voti=voti;
-            juur->vaartus=vaartus;
-        }else if(tipp->l==NULL && tipp->voti>voti){
-            tipp->l=new SplayNode();
-            tipp->l->voti=voti;
-            tipp->l->vaartus=vaartus;
-            tipp->l->p=tipp;
-            splay(tipp->l);
-        }else if(tipp->r==NULL && tipp->voti<voti){
-            tipp->r=new SplayNode();
-            tipp->r->voti=voti;
-            tipp->r->vaartus=vaartus;
-            tipp->r->p=tipp;
-            splay(tipp->r);
-        }else if(tipp->voti>voti)insert(tipp->l,voti,vaartus);
-        else if(tipp->voti<voti)insert(tipp->r,voti,vaartus);
-        else{
-            tipp->vaartus=vaartus;
-            splay(tipp);
-        }
-    }
-    SplayNode* parempoore(SplayNode* tipp){
-        auto y=tipp;
-        auto yp=tipp->p;
-        auto x=y->l;
-        auto T2=x->r;
-        x->r=y;
-        y->p=x;
-        y->l=T2;
-        if(T2!=NULL)
-            T2->p=y;
-        x->p=yp;
-        if(x->p!=NULL){
-            if(x->p->l==y)x->p->l=x;
-            else x->p->r=x;
-        }
-        return x;
-    }
-     SplayNode* vasakpoore(SplayNode* tipp){
-        auto x=tipp;
-        auto xp=tipp->p;
-        auto y=x->r;
-        auto T2=y->l;
-        y->l=x;
-        x->p=y;
-        x->r=T2;
-        if(T2!=NULL)
-            T2->p=x;
-        y->p=xp;
-        if(y->p!=NULL){
-            if(y->p->l==x)y->p->l=y;
-            else y->p->r=y;
-        }
-        return y;
-    }
-    void splay(SplayNode* tipp){
-        if(tipp==juur||tipp==NULL)return;
-        else if(tipp->p==juur){
-            if(tipp->p->l==tipp)juur=parempoore(juur);
-            else juur=vasakpoore(juur);
-        }else if(tipp->p->l==tipp && tipp->p->p->l==tipp->p){
-            if(tipp->p->p==juur){
-                juur=parempoore(juur);
-                juur=parempoore(juur);
-            }else{
-                tipp->p=parempoore(tipp->p->p);
-                tipp=parempoore(tipp->p);
-                splay(tipp);
-            }
-        }else if(tipp->p->r==tipp && tipp->p->p->r==tipp->p){
-            if(tipp->p->p==juur){
-                juur=vasakpoore(juur);
-                juur=vasakpoore(juur);
-            }else{
-                tipp->p=vasakpoore(tipp->p->p);
-                tipp=vasakpoore(tipp->p);
-                splay(tipp);
-            }
-        }else if(tipp->p->r==tipp&&tipp->p->p->l==tipp->p){
-            tipp=vasakpoore(tipp->p);
-            if(tipp->p==juur)juur=parempoore(juur);
-            else{
-                tipp=parempoore(tipp->p);
-                splay(tipp);
-            }
-        }else if(tipp->p->l==tipp && tipp->p->p->r==tipp->p){
-            tipp=parempoore(tipp->p);
-            if(tipp->p==juur)juur=vasakpoore(juur);
-            else{
-                tipp=vasakpoore(tipp->p);
-                splay(tipp);
-            }
-        }
-    }
-    void prindi(SplayNode* tipp){
-        if(tipp==NULL)return;
-        cout<<tipp->voti<<' '<<tipp->vaartus<<endl;
-        prindi(tipp->l);
-        prindi(tipp->r);
-    }
-public:
-    SplayTree(){
-        juur=NULL;
-    }
-    void prindi(){
-        prindi(juur);
-    }
-    void insert(K voti,V vaartus){
-        insert(juur,voti,vaartus);
-    }
-    void kustuta(K voti){
-        kustuta(juur,voti);
-    }
-    V otsi(K voti){
-        return otsi(juur,voti);
-    }
+template <typename T> struct SplayTree {
+	struct Node {
+		Node(){}
+		Node(T value_arg): value{value_arg} {}
+		T value{}; //!< Value associated with node
+		array<Node *, 2> child{}; //!< Left and right children
+		Node *parent{}; //!< Pointer to parent
+		//Node *path_parent{};
+		bool side() const {
+			/*! Returns true if child is on the right, and false otherwise*/
+			return parent->child[1] == this;
+		}
+		void rotate() {
+			/*! Rotate node x around its parent */
+			const auto p = parent;
+			const bool i = side();
+
+			if (p->parent) {
+				p->parent->attach(p->side(), this);
+			} else {
+				parent = nullptr;
+			}
+			p->attach(i, child[!i]);
+			attach(!i, p);
+		}
+		void splay() {
+			/*! Splay node x. x will become the root of the tree*/
+			for(;parent;rotate()) {
+				if (parent->parent) {
+					(side() == parent->side() ? parent: this)->rotate();
+				}
+			}
+		}
+		array<Node *, 2> split() {
+			splay();
+			const auto right = child[1];
+			if (right) {
+				right->parent = nullptr;
+			}
+			this->right = nullptr;
+			return {this, right};
+		}
+		void attach(bool side, Node *const new_) {
+			/*! Attach node new_ as the node's side children*/
+			if (new_) {
+				new_->parent = this;
+			}
+			child[side] = new_;
+		}
+	};
+	/** @brief Splay tree node */
+	/*! Splay tree iterator */
+	struct iterator {
+		using iterator_category = bidirectional_iterator_tag;
+		using value_type = T;
+		using pointer = T *;
+		using reference = T &;
+		using difference_type = ll;
+	      public:
+		void operator--() { advance<false>(); }
+		void operator++() { advance<true>(); }
+		const T &operator*() { return node->value; }
+		Node *node;
+		iterator(Node *node_arg) : node(node_arg) {}
+		bool operator==(const iterator oth) const {
+			return node == oth.node;
+		}
+		bool operator!=(const iterator oth) const {return !(*this == oth);}
+	      private:
+		template <bool dir> void advance() {
+			if (node->child[1]) {
+				node = extremum<!dir>(node->child[1]);
+				return;
+			}
+			for (; node->parent && node->side() == dir; node = node->parent)
+				;
+			node = node->parent;
+		}
+	};
+	Node *root{}; /*!< Root node */
+	ll size_{};   /*!< Size of the splay tree*/
+	SplayTree() {}
+	~SplayTree() { destroy(root); }
+	static void destroy(Node *const node) {
+		/*! Destroy the subtree node */
+		if (!node) {
+			return;
+		}
+		for (Node *const child : node->child) {
+			destroy(child);
+		}
+		delete node;
+	}
+	void insert(Node *const x) {
+		/*! Insert node x into the splay tree*/
+		++size_;
+		if (!root) {
+			root = x;
+			return;
+		}
+		auto y = root;
+		while (true) {
+			auto &nw = y->child[x->value > y->value];
+			if (!nw) {
+				nw = x;
+				nw->parent = y;
+				root = nw;
+				nw->splay();
+				return;
+			}
+			y = nw;
+		}
+		return;
+	}
+	void insert(const T &key) {
+		/*! Insert key key into the splay tree*/
+		insert(new Node{key});
+	}
+	void erase(Node *const x) {
+		/*! Erase node x from the splay tree*/
+		assert(x);
+		x->splay();
+		root = join(x->child[0], x->child[1]);
+		delete x;
+		--size_;
+	}
+	/** @brief Erases the node with key key from the splay tree */
+	void erase(const T &key) { erase(find(key)); }
+	template <bool i> static Node *extremum(Node * x) {
+		/*! Return the extremum of the subtree x. Minimum if i is false,
+		 * maximum if i is true.*/
+		assert(x);
+		for(; x->child[i]; x = x->child[i]);
+		return x;
+	}
+	static Node *join(Node *const a, Node *const b) {
+		if (!a) {
+			b->parent = nullptr;
+			return b;
+		}
+		Node *const mx = extremum<true>(a);
+		mx->splay();
+		assert(mx->child[1] == nullptr);
+		mx->child[1] = b;
+		mx->parent = nullptr;
+		return mx;
+	}
+    /*! Returns node with key key*/
+	Node *find(const T &key) {
+		auto x = root;
+		while(x && key != x->value){
+			const auto next = x->child[key > x->value];
+			if(!next){
+				x->splay();
+			}
+			x = next;
+		}
+		return x;
+	}
+	/**
+     * @brief Returns the number of nodes in the splay tree.
+     */
+    size_t size() const { return size_; }
+	bool empty() const { return size() == 0; }
+	iterator begin() { return iterator{extremum<false>(root)}; }
+	iterator end() { return iterator{nullptr}; }
 };
 int main()
 {ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     const int N=1000*100;
-    SplayTree<int,int>t;
+    SplayTree<int>t;
     for(int i=1;i<=N;i++){
-        t.insert(rand(),i);
+        t.insert(rand()*rand());
     }
     /*for(int i=1;i<=N;i++){
         t.kustuta(rand());
     }*/
-    t.prindi();
     return 0;
 }
