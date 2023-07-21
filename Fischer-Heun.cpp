a0226be792ddb39d53cf4252abfaea7154099f97
Jaagup Tamme <jaaguptamme@gmail.com>
2023-07-21 19:04:22 +0300

    Update Fischer-Heun.cpp

--- Fischer-Heun.cpp
+++ Fischer-Heun.cpp
@@ -1,94 +1,75 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 typedef long long ll;
 template<typename T>
 struct ST{
-    T* mn;
-    int* lg;
-    int n;
+    vector<vector<T>>mn;
+    vector<int>lg;
     ST(){}
     ST(vector<T>& A){
-        n=A.size();
-        lg=new int[n+1];
+        int n=A.size();
+        lg.resize(n+1,0);
         for(int i=1;i<=n;i++)lg[i]=log2(i);
         int m=log2(n);
-        mn=new T[(m+1)*n];
-        for(int i=0;i<n;i++)mn[i]=A[i];
+        mn.resize(m+1,vector<T>(n));
+        for(int i=0;i<n;i++)mn[0][i]=A[i];
         for(int j=1;j<=m;j++){
-            for(int i=0;i+(1<<j)<=n;i++)mn[j*n+i]=min(mn[(j-1)*n+i],mn[(j-1)*n+i+(1<<(j-1))]);
-        }
-    }
-    ST(T A[],int n){
-        this->n=n;
-        lg=new int[n+1];
-        for(int i=1;i<=n;i++)lg[i]=log2(i);
-        int m=log2(n);
-        mn=new T[(m+1)*n];
-        for(int i=0;i<n;i++)mn[i]=A[i];
-        for(int j=1;j<=m;j++){
-            for(int i=0;i+(1<<j)<=n;i++)mn[j*n+i]=min(mn[(j-1)*n+i],mn[(j-1)*n+i+(1<<(j-1))]);
+            for(int i=0;i+(1<<j)<=n;i++)mn[j][i]=min(mn[j-1][i],mn[j-1][i+(1<<(j-1))]);
         }
     }
     T get(int s,int e){
         int ln=lg[e+1-s];
-        return min(mn[ln*n+s],mn[ln*n+e+1-(1<<ln)]);
+        return min(mn[ln][s],mn[ln][e+1-(1<<ln)]);
     }
 };
 template<typename T>
 struct FH{
     ST<T> sparse_table;
     int b;
-    T* W;
-    unordered_map<int,int*>mns;
-    int* treenum;
-    T* dq;
+    vector<T>W;
+    unordered_map<int,vector<vector<int>>>mns;
+    vector<int>treenum;
     FH(vector<T>& vals){
+        W=vals;
         int n=vals.size();
-        W=new T[n];
-        for(int i=0;i<n;i++)W[i]=vals[i];
         b=max(1.0,(log2(n)+3)/4);
-        dq=new T[b];
         vector<T>current;
-        T* global=new T[n/b+(n%b!=0)];
-        int global_pos=0;
-        treenum=new int[n];
-        int treenum_cnt=0;
+        vector<T>global;
         for(int i=0;i<n;i++){
             current.push_back(vals[i]);
             if((i+1)%b==0 || i==n-1){
-                global[global_pos++]=*min_element(current.begin(),current.end());
+                global.push_back(*min_element(current.begin(),current.end()));
                 int vl=calc(current);
-                treenum[treenum_cnt++]=vl;
+                treenum.push_back(vl);
                 if(!mns.count(vl))build(current,vl);
-                current.clear();
+                current={};
             }
         }
-        sparse_table=ST<T>(global,global_pos);
+        sparse_table=ST<T>(global);
     }
-
     int calc(vector<T>& A){
-        int back=0;
+        deque<T>dq;
         int res=0;
         int place=2*b-1;
         for(auto el:A){
-            while(back&&dq[back-1]>el){
+            while(dq.size()&&dq.back()>el){
                 place--;
-                back--;
+                dq.pop_back();
             }
             res+=1<<place;
             place--;
-            dq[back++]=el;
+            dq.push_back(el);
         }
         return res;
     }
     void build(vector<T>& A,int vl){
-        int* R=new int[b*b];
-        //vector<vector<int>>R;
-        //A.resize(b);
-        //R.resize(b,vector<int>(b,0));
-        for(int i=0;i<b;i++)R[i*b+i]=i;
+        vector<vector<int>>R;
+        A.resize(b);
+        R.resize(b,vector<int>(b,0));
+        for(int i=0;i<b;i++)R[i][i]=i;
         for(int i=0;i<b;i++){
-            for(int j=i+1;j<b;j++)R[i*b+j]=A[R[i*b+j-1]]<A[j]?R[i*b+j-1]:j;
+            for(int j=i+1;j<b;j++)R[i][j]=A[R[i][j-1]]<A[j]?R[i][j-1]:j;
         }
         mns[vl]=R;
     }
@@ -99,15 +80,15 @@ struct FH{
             auto V=mns[treenum[bi]];
             int ii=i%b;
             int ij=j%b;
-            return W[bi*b+V[ii*b+ij]];
+            return W[bi*b+V[ii][ij]];
         }
         else if(bi+1==bj){
-            T left=W[bi*b+mns[treenum[bi]][(i%b)*b+b-1]];
-            T right=W[bj*b+mns[treenum[bj]][j%b]];
+            T left=W[bi*b+mns[treenum[bi]][i%b][b-1]];
+            T right=W[bj*b+mns[treenum[bj]][0][j%b]];
             return min(left,right);
-            }else{
-            T left=W[bi*b+mns[treenum[bi]][(i%b)*b+b-1]];
-            T right=W[bj*b+mns[treenum[bj]][j%b]];
+        }else{
+            T left=W[bi*b+mns[treenum[bi]][i%b][b-1]];
+            T right=W[bj*b+mns[treenum[bj]][0][j%b]];
             T mid=sparse_table.get(bi+1,bj-1);
             return min(left,min(mid,right));
         }
