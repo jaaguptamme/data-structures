233b76df65737be8d2622f731e71ae1bd1ceb17c
Jaagup Tamme <jaaguptamme@gmail.com>
2024-05-21 14:05:29 +0300

    Update JuhupöördusegaList.cpp

--- Mahutid/JuhupöördusegaList.cpp
+++ Mahutid/JuhupöördusegaList.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 //allikas: https://keithschwarz.com/interesting/code/?dir=random-access-list
+/*probleem: listis on ette elemendi lisamine ja eemaldamine konstantse keerukusega,
+            kuid indeksi järgi arvu otsimine on lineaarse keerukusega
+
+ lahendus: hoiame arve listina täielikest kahendpuudest, kusjuures kasutatavad suurused määrab skew binary numbrisüsteem
+           indeksi leidmine on nüüd logaritmilise keerukusega ning lisamine kustutamine endiselt konstantsed
+Mälulekke vaba
+
+*/
 typedef long long ll;
 using namespace std;
 template<typename T>
@@ -14,6 +22,12 @@ class JLL{
             cout<<vaartus<<' ';
             if(vasak!=nullptr)vasak->prindi();
             if(parem!=nullptr)parem->prindi();
+        }
+        T puusKsUusim(int k){//k vahemikus [0,suurus)
+            if(k==0)return vaartus;
+            int alampuu=(suurus-1)/2;
+            if(k<=alampuu)return vasak->puusKsUusim(k-1);
+            return parem->puusKsUusim(k-1-alampuu);
         }
     };
     using TP=shared_ptr<Tipp>;
@@ -45,7 +59,8 @@ public:
         }
     }
     static T viimane(JLL jll){
-        return jll.juured->tipp.vaartus;
+        if(onTuhi(jll))throw "TÜHI";
+        return jll.juured->tipp->vaartus;
     }
     static bool onTuhi(JLL jll){
         return jll.juured==nullptr;
@@ -60,8 +75,8 @@ public:
         return vastus;
     }
     static JLL eemaldaViimane(JLL jll){
-        if(jll->juured->tipp->suurus==1){
-            return JLL(jll->juured->edasi);
+        if(jll.juured->tipp->suurus==1){
+            return JLL(jll.juured->edasi);
         }else{
             return JLL(make_shared<List>(jll.juured->tipp->vasak,make_shared<List>(jll.juured->tipp->parem,jll.juured->edasi)));
         }
@@ -74,17 +89,31 @@ public:
         }
         cout<<endl;
     }
+    static T indeks(JLL jll,int k){//varaseim element indeksil 0
+        k=suurus(jll)-k-1;
+        shared_ptr<List>koht=jll.juured;
+        while(k>=koht->tipp->suurus){
+            k-=koht->tipp->suurus;
+            koht=koht->edasi;
+        }
+        return koht->tipp->puusKsUusim(k);
+    }
 };
 int main()
 {ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     using TUUP=JLL<int>;
-    const int N=1000*1000*10;
+    const int N=1000*1000;
     vector<TUUP>TS(N);
-    for(int t=0;t<200;t++){
+    for(int t=0;t<1;t++){
         for(int i=1;i<N;i++){
-            TS[i]=TUUP::lisaLoppu(TS[i-1],i);
+            TS[i]=TUUP::lisaLoppu(TS[(rand()*rand())%(i)],i);
+            //if(rand()%2)TS[i]=TUUP::eemaldaViimane(TS[i]);
         }
-        //TUUP::prindi(TS.back());
+        TUUP::prindi(TS.back());
+        for(int i=0;i<TUUP::suurus(TS.back());i++){
+            cout<<TUUP::indeks(TS.back(),i)<<' ';
+        }
+        cout<<endl;
         cout<<t<<endl;
     }
     return 0;
